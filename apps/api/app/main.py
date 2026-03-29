17980efa960f14375b3fe0efa0c62d31aace4632
dee <dee@DEE.dan>
2026-03-29 09:51:03 -0500

    Added cors: https://business-app-starter-template-web.vercel.app/

--- apps/api/app/main.py
+++ apps/api/app/main.py
@@ -12,10 +12,10 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://localhost:8000",
         "http://localhost:8081",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:8081",
+        "https://business-app-starter-template-web.vercel.app",
     ],
     allow_origin_regex=r"http://(localhost|127\.0\.0\.1):\d+",
     allow_credentials=True,
